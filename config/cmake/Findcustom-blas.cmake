2240085dc6a2d11bc7baa24a67de36bc97588de6
Daniel Mejia-Rodriguez <daniel.mejia@pnnl.gov>
2022-04-11 04:02:15 -0400

    New ILP64 interface

--- config/cmake/Findcustom-blas.cmake
+++ config/cmake/Findcustom-blas.cmake
@@ -14,10 +14,64 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with dftd4.  If not, see <https://www.gnu.org/licenses/>.
 
+function(check_blas)
+set(CMAKE_REQUIRED_FLAGS)
+set(CMAKE_REQUIRED_LINK_OPTIONS)
+set(CMAKE_REQUIRED_INCLUDES)
+set(CMAKE_REQUIRED_LIBRARIES ${LAPACK_LIBRARIES})
+check_fortran_source_compiles("
+      program check_blas
+      implicit none (type, external)
+      external :: dgemm
+      end program" BLAS_links)
+set(BLAS_links ${BLAS_links} PARENT_SCOPE)
+endfunction(check_blas)
+
+
 if(NOT BLAS_FOUND)
-  find_package("BLAS")
-  if(NOT TARGET "BLAS::BLAS")
-    add_library("BLAS::BLAS" INTERFACE IMPORTED)
-    target_link_libraries("BLAS::BLAS" INTERFACE "${BLAS_LIBRARIES}")
+
+  # Safeguard in case a vendor was specified
+  if(BLA_VENDOR MATCHES "_64ilp")
+    if(NOT WITH_ILP64)
+      message(STATUS "BLA_VENDOR ${BLA_VENDOR} uses 64-bit integers. Setting WITH_ILP64")
+      set(WITH_ILP64 ON PARENT_SCOPE)
+    endif()
+  endif()
+
+  # ILP64 check
+  if (WITH_ILP64 AND (NOT LAPACK_LIBRARIES))
+    if(NOT ((BLA_VENDOR MATCHES "_64ilp") OR (BLA_VENDOR MATCHES "_dyn")))
+      if(CMAKE_VERSION VERSION_LESS "3.22")
+        message(FATAL_ERROR
+          "Use of WITH_ILP64 with CMAKE_VERSION < 3.22 requires user to set BLA_VENDOR to a valid ilp64 provider or to set LAPACK_LIBRARIES")
+      elseif(NOT BLA_SIZEOF_INTEGER EQUAL 8)
+        set(BLA_SIZEOF_INTEGER 8 FORCE)
+      endif()
+    endif()
+  endif()
+
+  if(LAPACK_LIBRARIES)
+    # Check if BLAS links with the same set of libraries
+    if(NOT TARGET BLAS::BLAS)
+      check_blas()
+      if(BLAS_links)
+        set (BLAS_links ${BLAS_links})
+        add_library(BLAS::BLAS INTERFACE IMPORTED)
+        set_target_properties(BLAS::BLAS PROPERTIES
+          INTERFACE_LINK_LIBRARIES "${LAPACK_LIBRARIES}")
+      else()
+        message(FATAL_ERROR "Custom LAPACK library must contain BLAS")
+      endif()
+    endif()
+
+    set(BLAS_FOUND TRUE)
+
+  else()
+
+    find_package("BLAS")
+    if(NOT TARGET "BLAS::BLAS")
+      add_library("BLAS::BLAS" INTERFACE IMPORTED)
+      target_link_libraries("BLAS::BLAS" INTERFACE "${BLAS_LIBRARIES}")
+    endif()
   endif()
 endif()
--- config/cmake/Findmulticharge.cmake
+++ config/cmake/Findmulticharge.cmake
@@ -16,8 +16,8 @@
 
 set(_lib "multicharge")
 set(_pkg "MULTICHARGE")
-set(_url "https://github.com/grimme-lab/multicharge")
-set(_revision "v0.1.2")
+set(_url "https://github.com/dmejiar/multicharge")
+set(_revision "updates")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
